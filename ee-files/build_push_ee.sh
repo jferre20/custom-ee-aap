a89fe21b73a2e3d9f5e9a01135953d35a4b88895
Joao Ferreira <joaferre@redhat.com>
2024-12-17 18:39:30 -0300

    Create EE custom

--- ee-files/build_push_ee.sh
+++ ee-files/build_push_ee.sh
@@ -17,10 +17,10 @@ printf "\n"
 
 # ANSIBLE-NAVIGATOR TO CONFIGURE A CUSTOM EXECUTION ENVIRONMENT
 echo "CONFIGURING NEW EXECUTION ENVIRONMENT USING ANSIBLE CONTROLLER COLLECTION -----"
-ansible-navigator run -m stdout create_ee_aap.yml -e "ee_name=$EE_NEW_IMAGE ee_image=$EE_HUB_HOST/$EE_NEW_IMAGE" --eei $EE_HUB_HOST/$EE_TEST_IMAGE
+ansible-navigator run -m stdout ../create_ee_aap.yml -e "ee_name=$EE_NEW_IMAGE ee_image=$EE_HUB_HOST/$EE_NEW_IMAGE" --eei $EE_HUB_HOST/$EE_TEST_IMAGE
 printf "\n"
 
 # TESTING NEW EXECUTION ENVIRONMENT
 echo "VALIDATING NEW EE RUNNING A PLAYBOOK WITH CUSTOM COLLECTION -------------------"
-ansible-navigator run -m stdout mysql_info.yml -i inv -e "vmname=srv02" --eei $EE_HUB_HOST/$EE_NEW_IMAGE
+ansible-navigator run -m stdout ../mysql_info.yml -i inv -e "vmname=srv02" --eei $EE_HUB_HOST/$EE_NEW_IMAGE
 printf "\n"
